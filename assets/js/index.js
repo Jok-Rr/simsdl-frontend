3ba0a3b5971ddd8e5fa835d071d00d2603899f92
Tom <tom.bost.pro@gmail.com>
2023-02-03 10:19:25 +0100

    feat(Layout): double menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,36 @@
 //Toggle menu
-const burgerMenu = document.getElementById("burgermenu");
 const mobileMenu = document.getElementById("mobile-menu");
-const body = document.body;
-let target;
-
-document.addEventListener("click", (e) => {
-	target = e.target.id;
-});
-
-console.log(target);
-
+const accountMenu = document.getElementById("account-menu");
+const mainSection = document.getElementsByTagName("main")[0];
 function openMenuToggle() {
 	document.addEventListener("click", (event) => {
+		console.log(event.target.id);
 		if (event.target.id == "burgermenu") {
-			document.body.classList.toggle("overflow-hidden");
-			mobileMenu.classList.toggle("scale-y-100");
-		}
-		if (event.target.id !== "burgermenu") {
+			mobileMenu.classList.add("scale-y-100");
+			accountMenu.classList.remove("scale-y-100");
+			document.body.classList.add("overflow-hidden");
+		} else if (event.target.id == "user-icon") {
+			accountMenu.classList.add("scale-y-100");
 			mobileMenu.classList.remove("scale-y-100");
-			document.body.classList.remove("overflow-hidden");
+			document.body.classList.add("overflow-hidden");
 		}
+
+		if (
+			event.target.id !== "burgermenu" &&
+			event.target.id !== "user-icon" &&
+			event.target.parentNode.id !== "account-menu" &&
+			event.target.parentNode.id !== "mobile-menu"
+		) {
+			document.body.classList.remove("overflow-hidden");
+			document.body.classList.remove("blur-sm");
+			mobileMenu.classList.remove("scale-y-100");
+			accountMenu.classList.remove("scale-y-100");
+		}
+		// if (event.target.id !== "user-icon") {
+		// 	document.body.classList.remove("overflow-hidden");
+		// 	accountMenu.classList.remove("scale-y-100");
+		// }
 	});
 }
 
-// function closeMenuOutArea() {
-// 	body.addEventListener("click", () => {
-// 		mobileMenu.classList.add("hidden");
-// 	});
-// }
-
 openMenuToggle();
-// closeMenuOutArea();
