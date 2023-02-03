d323ef72ddbf5b4d353bdb1bab906ec936737dc4
Tom <tom.bost.pro@gmail.com>
2023-02-03 10:34:10 +0100

    fix(layout): menu toggle

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,17 +2,35 @@
 const mobileMenu = document.getElementById("mobile-menu");
 const accountMenu = document.getElementById("account-menu");
 const mainSection = document.getElementsByTagName("main")[0];
+const burgerButton = document.getElementById("burgermenu");
+
 function openMenuToggle() {
 	document.addEventListener("click", (event) => {
 		console.log(event.target.id);
 		if (event.target.id == "burgermenu") {
-			mobileMenu.classList.add("scale-y-100");
-			accountMenu.classList.remove("scale-y-100");
-			document.body.classList.add("overflow-hidden");
+			if (document.body.classList.contains("overflow-hidden")) {
+				burgerButton.classList.add("fa-bars");
+				burgerButton.classList.remove("fa-xmark-large");
+				mobileMenu.classList.remove("scale-y-100");
+				accountMenu.classList.remove("scale-y-100");
+				document.body.classList.remove("overflow-hidden");
+			} else {
+				burgerButton.classList.remove("fa-bars");
+				burgerButton.classList.add("fa-xmark-large");
+				mobileMenu.classList.add("scale-y-100");
+				accountMenu.classList.remove("scale-y-100");
+				document.body.classList.add("overflow-hidden");
+			}
 		} else if (event.target.id == "user-icon") {
-			accountMenu.classList.add("scale-y-100");
-			mobileMenu.classList.remove("scale-y-100");
-			document.body.classList.add("overflow-hidden");
+			if (document.body.classList.contains("overflow-hidden")) {
+				accountMenu.classList.remove("scale-y-100");
+				mobileMenu.classList.remove("scale-y-100");
+				document.body.classList.remove("overflow-hidden");
+			} else {
+				accountMenu.classList.add("scale-y-100");
+				mobileMenu.classList.remove("scale-y-100");
+				document.body.classList.add("overflow-hidden");
+			}
 		}
 
 		if (
@@ -26,10 +44,6 @@ function openMenuToggle() {
 			mobileMenu.classList.remove("scale-y-100");
 			accountMenu.classList.remove("scale-y-100");
 		}
-		// if (event.target.id !== "user-icon") {
-		// 	document.body.classList.remove("overflow-hidden");
-		// 	accountMenu.classList.remove("scale-y-100");
-		// }
 	});
 }
 
