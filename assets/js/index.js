9133be9acf61469d6197800ca4ed29552ffc98d1
Tom <tom.bost.pro@gmail.com>
2023-02-14 09:26:29 +0100

    feat(layout): modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,11 @@ const accountButton = document.getElementById("user-icon");
 const menuBox = document.getElementById("mobile-menu");
 const accountBox = document.getElementById("account-menu");
 
+const btnModal = document.getElementById("btn-modal-open");
+const parentModal = document.getElementById("modal-parent");
+const boxModal = document.getElementById("modal-box");
+const closeModalbtn = document.getElementById("close-modal");
+
 const toggleMenu = function () {
 	if (accountBox.classList.contains("scale-y-100")) {
 		menuButton.classList.add("opened");
@@ -39,11 +44,38 @@ accountButton.addEventListener("click", function (e) {
 	toggleAccount();
 });
 
-document.documentElement.addEventListener("click", function () {
+document.documentElement.addEventListener("click", function (e) {
 	if (accountBox.classList.contains("scale-y-100")) {
 		toggleAccount();
 	}
 	if (menuBox.classList.contains("scale-y-100")) {
 		toggleMenu();
 	}
+});
+
+const openModal = () => {
+	parentModal.classList.remove("hidden");
+	setTimeout(() => {
+		boxModal.classList.add("scale-y-100");
+	}, 100);
+	document.documentElement.classList.add("overflow-hidden");
+};
+
+const closeModal = () => {
+	boxModal.classList.remove("scale-y-100");
+	setTimeout(() => {
+		parentModal.classList.add("hidden");
+	}, 100);
+
+	document.documentElement.classList.remove("overflow-hidden");
+};
+
+btnModal.addEventListener("click", (e) => {
+	e.stopPropagation();
+	openModal();
+});
+
+closeModalbtn.addEventListener("click", (e) => {
+	e.stopPropagation();
+	closeModal();
 });
