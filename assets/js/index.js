ace412c4af42465b16e457769e0b101e359ca5a4
Tom <tom.bost.pro@gmail.com>
2023-02-14 16:47:56 +0100

    fix(Modal): flex to grid

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,73 +9,76 @@ const parentModal = document.getElementById("modal-parent");
 const boxModal = document.getElementById("modal-box");
 const closeModalbtn = document.getElementById("close-modal");
 
-const toggleMenu = function () {
-	if (accountBox.classList.contains("scale-y-100")) {
-		menuButton.classList.add("opened");
+if (menuButton && accountButton && menuBox && accountBox) {
+	const toggleMenu = function () {
+		if (accountBox.classList.contains("scale-y-100")) {
+			menuButton.classList.add("opened");
 
-		accountBox.classList.remove("scale-y-100");
-		menuBox.classList.toggle("scale-y-100");
-	} else {
-		menuButton.classList.toggle("opened");
-		menuBox.classList.toggle("scale-y-100");
-		document.documentElement.classList.toggle("overflow-hidden");
-	}
-};
+			accountBox.classList.remove("scale-y-100");
+			menuBox.classList.toggle("scale-y-100");
+		} else {
+			menuButton.classList.toggle("opened");
+			menuBox.classList.toggle("scale-y-100");
+			document.documentElement.classList.toggle("overflow-hidden");
+		}
+	};
 
-const toggleAccount = function () {
-	if (menuBox.classList.contains("scale-y-100")) {
-		menuButton.classList.remove("opened");
+	const toggleAccount = function () {
+		if (menuBox.classList.contains("scale-y-100")) {
+			menuButton.classList.remove("opened");
 
-		menuBox.classList.remove("scale-y-100");
-		accountBox.classList.toggle("scale-y-100");
-	} else {
-		accountBox.classList.toggle("scale-y-100");
-		document.documentElement.classList.toggle("overflow-hidden");
-	}
-};
+			menuBox.classList.remove("scale-y-100");
+			accountBox.classList.toggle("scale-y-100");
+		} else {
+			accountBox.classList.toggle("scale-y-100");
+			document.documentElement.classList.toggle("overflow-hidden");
+		}
+	};
 
-menuButton.addEventListener("click", function (e) {
-	e.stopPropagation();
-	toggleMenu();
-});
-
-accountButton.addEventListener("click", function (e) {
-	e.stopPropagation();
-	toggleAccount();
-});
-
-document.documentElement.addEventListener("click", function (e) {
-	if (accountBox.classList.contains("scale-y-100")) {
-		toggleAccount();
-	}
-	if (menuBox.classList.contains("scale-y-100")) {
+	menuButton.addEventListener("click", function (e) {
+		e.stopPropagation();
 		toggleMenu();
-	}
-});
+	});
 
-const openModal = () => {
-	parentModal.classList.remove("hidden");
-	setTimeout(() => {
-		boxModal.classList.add("scale-y-100");
-	}, 100);
-	document.documentElement.classList.add("overflow-hidden");
-};
+	accountButton.addEventListener("click", function (e) {
+		e.stopPropagation();
+		toggleAccount();
+	});
+	document.documentElement.addEventListener("click", function (e) {
+		if (accountBox.classList.contains("scale-y-100")) {
+			toggleAccount();
+		}
+		if (menuBox.classList.contains("scale-y-100")) {
+			toggleMenu();
+		}
+	});
+}
 
-const closeModal = () => {
-	boxModal.classList.remove("scale-y-100");
-	setTimeout(() => {
-		parentModal.classList.add("hidden");
-	}, 100);
+if (btnModal && parentModal && boxModal && closeModalbtn) {
+	const openModal = () => {
+		parentModal.classList.remove("hidden");
+		setTimeout(() => {
+			boxModal.classList.add("scale-y-100");
+		}, 100);
+		document.documentElement.classList.add("overflow-hidden");
+	};
 
-	document.documentElement.classList.remove("overflow-hidden");
-};
+	const closeModal = () => {
+		boxModal.classList.remove("scale-y-100");
+		setTimeout(() => {
+			parentModal.classList.add("hidden");
+		}, 100);
 
-btnModal.addEventListener("click", (e) => {
-	e.stopPropagation();
-	openModal();
-});
+		document.documentElement.classList.remove("overflow-hidden");
+	};
 
-closeModalbtn.addEventListener("click", (e) => {
-	e.stopPropagation();
-	closeModal();
-});
+	btnModal.addEventListener("click", (e) => {
+		e.stopPropagation();
+		openModal();
+	});
+
+	closeModalbtn.addEventListener("click", (e) => {
+		e.stopPropagation();
+		closeModal();
+	});
+}
